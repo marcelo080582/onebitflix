d67d0809701ffa2ecd96a63439d338befb018476
Marcelo Martins <marcelo.martins@cilia.com.br>
2021-05-14 14:16:05 -0300

    preparing the models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :trackable, :validatable
+  has_many :reviews
+  has_many :favorites
+  has_many :players
+  validates :name, presence: true, on: :update
 end
